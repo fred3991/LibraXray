d3a327f6b581ee653a06d5c525aee50fb2ce2da8
Федоров Евгений Андреевич <Viva_la_lumiere@mail.ru>
2023-09-03 21:16:04 +0200

    libra fix

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -73,11 +73,17 @@ namespace LibraXray.ViewModels
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
             }
             CommandManager.InvalidateRequerySuggested();
         });
 
+        public void LogMessageString(string m)
+        {
+            LogStrings.Insert(0, DateTime.Now.ToString("[MM/dd/yyyy HH:mm:ss] ") + RecivedCommand);
+            if (LogStrings.Count > 100)
+                LogStrings.RemoveAt(LogStrings.Count - 1);
+        }
         private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
         {
             SerialPort sp = (SerialPort)sender;
@@ -86,9 +92,7 @@ namespace LibraXray.ViewModels
 
             try
             {
-                LogStrings.Insert(0, DateTime.Now.ToString("[MM/dd/yyyy HH:mm:ss] ") + RecivedCommand);
-                if (LogStrings.Count > 100)
-                    LogStrings.RemoveAt(LogStrings.Count - 1);
+                LogMessageString(RecivedCommand);
             }
             catch
             {
@@ -110,7 +114,7 @@ namespace LibraXray.ViewModels
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
             }
             CommandManager.InvalidateRequerySuggested();
         });
@@ -146,10 +150,178 @@ namespace LibraXray.ViewModels
             }
             catch (Exception ex)
             {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            CommandManager.InvalidateRequerySuggested();
+        });
+
+
+        public ICommand _BeepXrayCommand;
+        public ICommand BeepXrayCommand => _BeepXrayCommand ??= new RelayCommand(parameter =>
+        {
+            try
+            {
+                Console.Beep(800, 500);
+            }
+            catch (Exception ex)
+            {
                 MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
             }
             CommandManager.InvalidateRequerySuggested();
         });
+
+        #region VoltageControl
+
+        private string _AcitveVoltage = "0";
+        public string AcitveVoltage
+        {
+            get => _AcitveVoltage;
+            set => Set(ref _AcitveVoltage, value);
+        }
+
+        private string _TubeVoltage = "0";
+        public string TubeVoltage
+        {
+            get => _TubeVoltage;
+            set => Set(ref _TubeVoltage, value);
+        }
+
+        private string _SetVoltage = "40";
+        public string SetVoltage
+        {
+            get => _SetVoltage;
+            set => Set(ref _SetVoltage, value);
+        }
+
+        public ICommand _UpVoltageCommand;
+        public ICommand UpVoltageCommand => _UpVoltageCommand ??= new RelayCommand(parameter =>
+        {
+            try
+            {
+                var currentVoltage = Convert.ToInt32(SetVoltage);
+                currentVoltage++;
+
+                if (currentVoltage >= 150)
+                {
+                    currentVoltage = 150;
+                }
+
+                SetVoltage = currentVoltage.ToString();
+                UpdateXraySetting();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            CommandManager.InvalidateRequerySuggested();
+        });
+
+        public ICommand _DownVoltageCommand;
+        public ICommand DownVoltageCommand => _DownVoltageCommand ??= new RelayCommand(parameter =>
+        {
+            try
+            {
+                var currentVoltage = Convert.ToInt32(SetVoltage);
+                currentVoltage--;
+
+                if (currentVoltage <= 40)
+                {
+                    currentVoltage = 40;
+                }
+
+                SetVoltage = currentVoltage.ToString();
+                UpdateXraySetting();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            CommandManager.InvalidateRequerySuggested();
+        });
+
+        #endregion
+
+        #region CurrentControl
+
+        private string _AcitveCurrent = "0";
+        public string AcitveCurrent
+        {
+            get => _AcitveCurrent;
+            set => Set(ref _AcitveCurrent, value);
+        }
+
+        private string _TubeCurrent = "0";
+        public string TubeCurrent
+        {
+            get => _TubeCurrent;
+            set => Set(ref _TubeCurrent, value);
+        }
+
+        private string _SetCurrent = "10";
+        public string SetCurrent
+        {
+            get => _SetCurrent;
+            set => Set(ref _SetCurrent, value);
+        }
+
+        public ICommand _UpCurrentCommand;
+        public ICommand UpCurrentCommand => _UpCurrentCommand ??= new RelayCommand(parameter =>
+        {
+            try
+            {
+                var currentCurrent = Convert.ToInt32(SetCurrent);
+                currentCurrent++;
+
+                if (currentCurrent >= 500)
+                {
+                    currentCurrent = 500;
+                }
+
+                SetCurrent = currentCurrent.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            CommandManager.InvalidateRequerySuggested();
+        });
+
+        public ICommand _DownCurrentCommand;
+        public ICommand DownCurrentCommand => _DownCurrentCommand ??= new RelayCommand(parameter =>
+        {
+            try
+            {
+                var currentCurrent = Convert.ToInt32(SetCurrent);
+                currentCurrent--;
+
+                if (currentCurrent <= 10)
+                {
+                    currentCurrent = 10;
+                }
+
+                SetCurrent = currentCurrent.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            CommandManager.InvalidateRequerySuggested();
+        });
+
+        #endregion
+
+        public void UpdateXraySetting()
+        {
+            //Set Voltage
+            var strVoltage = LibraXrayCommands.HIV + String.Format(" {0:000}", SetVoltage);
+            sp232.WriteLine(strVoltage);
+
+            var strCurrent = LibraXrayCommands.CUR + String.Format(" {0:000}", SetCurrent);
+            sp232.WriteLine(strCurrent);
+
+            LogMessageString(RecivedCommand);
+            //sp232.WriteLine(LibraXrayCommands.HIV + String.Format("%02d", SetVoltage));
+        }
 
 
         public MainWindowViewModel()
